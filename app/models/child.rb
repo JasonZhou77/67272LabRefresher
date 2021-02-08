a0535284eaa17658a1ab5c19c4346d23208bd2da
JasonZhou <zesenzhou711@gmail.com>
2021-02-08 10:12:09 -0500

    all passing for child

--- app/models/child.rb
+++ app/models/child.rb
@@ -3,5 +3,12 @@ class Child < ApplicationRecord
 	has_many :tasks, through: :chores
 
 	validates_presence_of :first_name, :last_name
+
+	def name
+  		first_name + " " + last_name
+	end
+
+	scope :alphabetical, -> { order('last_name, first_name') }
+	scope :active, -> { where(active: true) }
     
 end
